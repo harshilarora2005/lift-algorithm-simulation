18a4823347a726d4b8a4ec1c40faccd797e52eb2
harshilarora2005 <harshil.arora205@gmail.com>
2024-10-30 00:25:35 +0530

    added scan & look

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const liftavailable=new Map();
 const liftlocation=new Map();
 const liftQueues=new Map();
 let liftscount,floorscount;
-let algorithm="Scan or Look";
+let algorithm="Scan";
 
 function toggle(x){
     const element=document.querySelector(`#option input:nth-of-type(${x})`);
@@ -122,9 +122,9 @@ document.getElementById("submit-destination").onclick = function() {
 
 function addLiftRequest(origin,destination){
     switch(algorithm){
-        case 'Scan or Look':
+        case 'Scan':
             algorithm1(origin,destination);
-        case 'Destination Control':
+        case 'Look':
             algorithm2(origin,destination);
         case 'Shortest Seek Time First':
             algorithm3(origin,destination);
@@ -133,14 +133,250 @@ function addLiftRequest(origin,destination){
     }
 }
 function algorithm1(origin,destination){
-    //for SCAN OR LOOK
+    //for SCAN 
 }
 function algorithm2(origin,destination){
-    //for DESTINATION CONTROL
+    //for LOOK
 }
 function algorithm3(origin,destination){
     //for SHORTEST SEEK TIME FIRST
 }
 function algorithm4(origin,destination){
     //for COLLECTIVE CONTROL
+}
+const liftCurrentLocations = new Map(); 
+const liftQueue = [];
+let floorsPerBuilding;
+function startComparison() {
+    document.getElementById('landing').style.display = 'none';
+    document.getElementById('comparison-area').style.display = 'block';
+    const numBuildings = 4; 
+    floorsPerBuilding =  Math.floor(Math.random() * (25 - 10 + 1)) + 10; 
+    const floorsCountContainer = document.querySelector("#floors-count");
+    floorsCountContainer.textContent=`Floors count - ${floorsPerBuilding}`;
+    const buildingsContainer = document.getElementById("buildings-container");
+    buildingsContainer.innerHTML = "";
+    for (let i = 1; i <= numBuildings; i++) {
+        const building = document.createElement("div");
+        building.classList.add("building");
+        building.id = `building-${i}`;
+        liftCurrentLocations.set(`lifts-${i}`, 0);
+        // Create floors
+        for (let j = floorsPerBuilding; j > 0; j--) {
+            const floor = document.createElement("div");
+            floor.classList.add("floors");
+            floor.innerHTML = `
+                <div class="floors-details">
+                    <p class="floors-number">Floor-${j}</p>
+                </div>
+            `;
+            building.appendChild(floor);
+        }
+        const groundFloor = document.createElement("div");
+        groundFloor.className = "floors";
+        groundFloor.id = `floors-0`;
+        groundFloor.innerHTML = `
+            <div class="floors-details">
+                <p class="floors-number">Floor-0</p> 
+            </div>
+        `;
+        building.appendChild(groundFloor);
+        const lift = document.createElement("div");
+        lift.className = "lifts";
+        lift.id = `lifts-${i}`;
+        lift.innerHTML = `
+            <div class='doors left-doors'></div>
+            <div class='doors right-doors'></div>
+        `;
+
+        building.appendChild(lift);
+        buildingsContainer.appendChild(building);
+    }
+    Promise.all([
+        generateRandomLiftRequest('lifts-1', floorsPerBuilding),
+        generateRandomLiftRequest('lifts-2', floorsPerBuilding)
+    ]).then(() => {
+        console.log("Both lift requests processed.");
+    });
+}
+
+function generateRandomLiftRequest(liftid, numfloors) {
+    return new Promise((resolve) => {
+        liftQueue = []; 
+        for (let i = 0; i < 5; i++) {
+            const origin = Math.floor(Math.random() * (numfloors + 1)); 
+            let destination;
+            do {
+                destination = Math.floor(Math.random() * (numfloors + 1)); 
+            } while (destination === origin);
+            liftQueue.push({ origin, destination });
+        }
+        const requestContainer = document.getElementById("requests-count");
+        requestContainer.innerHTML = ''; 
+        liftQueue.forEach((request, index) => {
+            const requestItem = document.createElement('div');
+            requestItem.textContent = `Request ${index + 1}: Origin - ${request.origin}, Destination - ${request.destination}`;
+            requestContainer.appendChild(requestItem);
+        });
+        console.log(liftQueue);
+        processLiftRequests(liftid);
+        resolve();
+    });
+}
+
+function scanAlgorithm(liftId) {
+    const currentLocation = liftCurrentLocations.get(liftId);
+    let path = [];
+    const requests = liftQueue.map(request => ({
+        origin: request.origin,
+        destination: request.destination
+    }));
+    for (let i = 0; i < requests.length; i++) {
+        for (let j = 0; j < requests.length - 1 - i; j++) {
+            if (requests[j].origin> requests[j + 1].origin) {
+                const temp = requests[j];
+                requests[j] = requests[j + 1];
+                requests[j + 1] = temp;
+            }
+        }
+    }
+    const origins = requests.map(request => request.origin);
+    let destinations = requests.map(request => request.destination);
+    for(let i=0;i<5;i++){
+        path.push(origins[i]);
+        if(destinations[i]>origins[i]){
+            path.push(destinations[i]);
+            destinations = destinations.filter(item => item !== destinations[i])
+        }
+    }
+    path = [...new Set(path)];
+    path.sort((a, b) => a - b);
+    path.push(floorsPerBuilding);
+    destinations.reverse();
+    path.push()
+    for (const destination of destinations) {
+        path.push(destination);
+    }
+    return path;
+}
+function lookAlgorithm(liftId) {
+    const currentLocation = liftCurrentLocations.get(liftId);
+    let path = [];
+    const requests = liftQueue.map(request => ({
+        origin: request.origin,
+        destination: request.destination
+    }));
+    for (let i = 0; i < requests.length; i++) {
+        for (let j = 0; j < requests.length - 1 - i; j++) {
+            if (requests[j].origin> requests[j + 1].origin) {
+                const temp = requests[j];
+                requests[j] = requests[j + 1];
+                requests[j + 1] = temp;
+            }
+        }
+    }
+    const origins = requests.map(request => request.origin);
+    let destinations = requests.map(request => request.destination);
+    for(let i=0;i<5;i++){
+        path.push(origins[i]);
+        if(destinations[i]>origins[i]){
+            path.push(destinations[i]);
+            destinations = destinations.filter(item => item !== destinations[i])
+        }
+    }
+    path = [...new Set(path)];
+    path.sort((a, b) => a - b);
+    destinations.reverse();
+    path.push()
+    for (const destination of destinations) {
+        path.push(destination);
+    }
+    return path;
+}
+function processLiftRequests(liftId) {
+    const choice=liftId.split("-")[1];
+    if (liftQueue.length > 0) {
+        switch(choice){
+            case 1:{
+                const path = scanAlgorithm(liftId); 
+                if (path) {
+                    moveLift(liftId, path);
+                }
+                break;
+            }
+            case 2:{
+                const path = lookAlgorithm(liftId); 
+                if (path) {
+                    moveLift(liftId, path);
+                }
+                break;
+            }
+            case 3:{
+                
+                break;
+            }
+            case 4:{
+                
+                break;
+            }
+            default:
+                console.log("end");
+                break;
+        }
+    }
+}
+
+function moveLift(liftId, destinations) {
+    const liftDiv = document.getElementById(liftId);
+    let currentFloorIndex = 0; 
+    function processNextDestination() {
+        if (currentFloorIndex < destinations.length) {
+            const destinationFloor = destinations[currentFloorIndex];
+            const newPosition = destinationFloor * 110; 
+            liftDiv.style.transition = 'transform 5s'; 
+            liftDiv.style.transform = `translateY(-${newPosition}px)`; 
+            setTimeout(() => {
+                liftCurrentLocations.set(liftId, destinationFloor); 
+                console.log(`${liftId} has arrived at floor ${destinationFloor}.`);
+                openLiftDoors(liftId);
+                currentFloorIndex++;
+                setTimeout(processNextDestination, 3000); 
+            }, 5000); 
+        } else {
+            const returnFloor = 0;
+            const newPosition = returnFloor * 110; 
+            liftDiv.style.transition = 'transform 2s'; 
+            liftDiv.style.transform = `translateY(-${newPosition}px)`; 
+
+            setTimeout(() => {
+                liftCurrentLocations.set(liftId, returnFloor); 
+                console.log(`${liftId} has returned to floor 0.`);
+                closeLiftDoors(liftId); 
+            }, 2000); 
+        }
+    }
+    processNextDestination(); 
+}
+function openLiftDoors(liftId) {
+    const liftDiv = document.getElementById(liftId);
+    const leftDoor = liftDiv.querySelector('.left-doors');
+    const rightDoor = liftDiv.querySelector('.right-doors');
+    leftDoor.style.transition = 'transform 2s'; 
+    rightDoor.style.transition = 'transform 2s'; 
+    leftDoor.style.transform = 'translateX(-50%)'; 
+    rightDoor.style.transform = 'translateX(50%)'; 
+    console.log(`Doors of ${liftId} are opening...`);
+    setTimeout(() => {
+        closeLiftDoors(liftId);
+    }, 3000); 
+}
+function closeLiftDoors(liftId) {
+    const liftDiv = document.getElementById(liftId);
+    const leftDoor = liftDiv.querySelector('.left-doors');
+    const rightDoor = liftDiv.querySelector('.right-doors');
+    leftDoor.style.transition = 'transform 0.5s'; 
+    rightDoor.style.transition = 'transform 0.5s'; 
+    leftDoor.style.transform = 'translateX(0)'; 
+    rightDoor.style.transform = 'translateX(0)'; 
+    console.log(`Doors of ${liftId} are closing...`);
 }
